d57f2c92abb5d113eca72b94a78e84fbb0b91f30
mamej <mamej@MejoradaMarin>
2024-03-09 21:55:55 +0100

    SUBIDA

--- src/tests/FuncionesRecursivasTest.java
+++ src/tests/FuncionesRecursivasTest.java
@@ -1,14 +1,62 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import clases.FuncionesRecursivas;
 
 class FuncionesRecursivasTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	@ParameterizedTest
+	@MethodSource("sumatorio")
+	void testSumatorio(int num, int esperado) {
+		int res = FuncionesRecursivas.sumatorio(num);
+		assertEquals(esperado, res);
+	}
+
+	private static Stream<Arguments> sumatorio() {
+		return Stream.of(
+				Arguments.of(5, 15),
+				Arguments.of(3, 6),
+				Arguments.of(1, 1)
+				);
+	}
+	
+	
+	@ParameterizedTest
+	@MethodSource("potencia")
+	void testPotencia(double num, int potencia, int esperado) {
+		double res = FuncionesRecursivas.potencia(num,potencia);
+		assertEquals(esperado, res);
+	}
+
+	private static Stream<Arguments> potencia() {
+		return Stream.of(
+				Arguments.of(2, 2, 4),
+				Arguments.of(5, 2, 25),
+				Arguments.of(2, 5, 32)
+				);
+	}
+	
+	
+	@ParameterizedTest
+	@MethodSource("fibonacci")
+	void testFibonacci(int num, int esperado) {
+		int res = FuncionesRecursivas.fibonacci(num);
+		assertEquals(esperado, res);
+	}
+
+	private static Stream<Arguments> fibonacci() {
+		return Stream.of(
+				Arguments.of(6, 8),
+				Arguments.of(20, 6765),
+				Arguments.of(10, 55)
+				);
 	}
 
 }
--- src/tests/TablasTest.java
+++ src/tests/TablasTest.java
@@ -1,14 +1,35 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import clases.Tablas;
 
 class TablasTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	@ParameterizedTest
+	@MethodSource("sumatorio")
+	void testSumatorio(int[][] matriz, boolean esperado) {
+		boolean res = Tablas.esMagica(matriz);
+		assertEquals(esperado, res);
 	}
+
+	private static Stream<Arguments> sumatorio() {
+		int[][] matrizMagica = { { 8, 3, 4 }, { 1, 5, 9 }, { 6, 7, 2 } };
+		int[][] matrizNoMagica = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+		
+		return Stream.of(
+				Arguments.of(matrizMagica, true),
+				Arguments.of(matrizNoMagica, false)
+				);
+	}
+	
+	
+	
 
 }
